66b76dc78e39e2714f4e766d31fecf345f4a1c68
Isaac Gros <isaac.gros888@gmail.com>
2020-03-04 16:15:39 +0100

    Configuration basique

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig', [
+            'title' => "Hello world !"
+        ]);
+    }
+}
