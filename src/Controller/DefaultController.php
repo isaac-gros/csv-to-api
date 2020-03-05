c917e9761cf324ed66c3cdbc4a56c42ffd7a5c98
Isaac Gros <isaac.gros888@gmail.com>
2020-03-05 15:19:57 +0100

    Traitement de fichiers CSV et affichage des résultats en accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 use Doctrine\ODM\MongoDB\DocumentManager as DocumentManager;
-use App\Document\Upload;
+use App\Document\CsvApi;
+use PhpOffice\PhpSpreadsheet\Reader\Csv;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 
 
 class DefaultController extends AbstractController
@@ -14,26 +17,71 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function indexAction()
+    public function indexAction(DocumentManager $dm)
     {
+        $apis = $dm->getRepository(CsvApi::class)->findAll();
         return $this->render('base.html.twig', [
-            'title' => "Hello world !"
+            'title' => "Mes APIs",
+            'apis' => $apis
         ]);
     }
 
     /**
      * @Route("/new", name="read_csv", methods={"POST","HEAD"})
      */
-    public function readCsvAction(DocumentManager $dm)
+    public function readCsvAction(Request $request, DocumentManager $dm)
     {
-        $upload = new Upload();
-        $upload->setName("Hello world");
+        // Retrieve file path
+        $uploads_dir = $this->getParameter('uploads_directory');
+        $csv_file = $uploads_dir . "dummy.csv";
         
-        $dm->persist($upload);
+        // CSV parser
+        $reader = new Csv();
+        $loader = $reader->load($csv_file);
+        $sheet = $loader->getActiveSheet();
+        $collection = $sheet->getCellCollection();
+        $cell_coordinates = $collection->getCoordinates();
+
+        // Final JSON
+        $api_name = (!empty($request->request->get("name"))) ? $request->request->get("name") : "Mon API";
+        $json = [
+            "name" => $api_name
+        ];
+
+        foreach($cell_coordinates as $coordinate) {
+            $cell = $collection->get($coordinate);
+            $current_column = $cell->getColumn();
+            $current_row = $cell->getRow();
+
+            if($current_row > 1) {
+                $key_coordinate = $current_column . 1;
+                $key_name = $collection->get($key_coordinate)->getValue();
+                $json["data"][$current_row -2][$key_name] = $cell->getValue();
+            }
+        }
+
+        $new_api = new CsvApi();
+        $new_api->setName($api_name);
+        $new_api->setContent(json_encode($json));
+        $dm->persist($new_api);
         $dm->flush();
 
-        return $this->redirectToRoute("index", [
-            'title' => "Sent !"
-        ]);
+        return $this->redirectToRoute("index");
+    }
+
+    /**
+     * @Route("/api/{id}", name="get_api", methods={"GET","HEAD"})
+     */
+    public function readApiAction($id, DocumentManager $dm)
+    {
+        $api = $dm->getRepository(CsvApi::class)->findOneBy(["_id" => $id]);
+        $api_content = $api->getContent();
+
+        $response = new Response();
+        $response->setContent($api_content);
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
--- src/Document/CsvApi.php
+++ src/Document/CsvApi.php
@@ -6,9 +6,9 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
- * @MongoDB\Document
+ * @MongoDB\Document(collection="APIs")
  */
-class Upload
+class CsvApi
 {
     /**
      * @MongoDB\Id
@@ -20,14 +20,35 @@ class Upload
      */
     protected $name;
 
+     /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $content;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
     public function setName($name)
     {
         $this->name = $name;
         return $this;
     }
 
-    public function getName($name)
+    public function getName()
     {
         return $this->name;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
     }
 }
